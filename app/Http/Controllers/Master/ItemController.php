d95dd57fb6b07524b82b9da42f7a44fed7761bf3
sellayspy <riki.fauziagaming@gmail.com>
2025-10-16 11:12:56 +0700

    penambahan fitur update dan show

--- app/Http/Controllers/Master/ItemController.php
+++ app/Http/Controllers/Master/ItemController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Master;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Master\ItemRequest;
+use App\Http\Requests\Master\UpdateRequest;
 use App\Models\Master\Item;
 use App\Models\User;
 use Exception;
@@ -34,6 +35,29 @@ class ItemController extends Controller
         }
     }
 
+    public function show(Item $item)
+    {
+        try {
+            $user = Auth::user();
+
+            if ($user->role !== 'admin' && $user->id !== $item->user_id) {
+                return response()->json([
+                    'status'    => 'gagal ambil data',
+                    'message'   => 'anda tidak bisa melihat data ini'
+                ], 403);
+            }
+
+            $item->load(['user']);
+            return response()->json([
+                'status' => 'sukses ambil data',
+                'data' => $item
+            ]);
+
+        } catch (\Thr) {
+            //throw $th;
+        }
+    }
+
     public function store(ItemRequest $request)
     {
         try {
@@ -55,6 +79,29 @@ class ItemController extends Controller
             ], 500);
         }
     }
+
+
+    public function update(UpdateRequest $request, Item $item)
+    {
+        try {
+            $validated = $request->safe()->all();
+            $validated['purchase_date'] = $validated['purchase_date'] ?? $item->purchase_date->toDateString();
+            $validated['user_id'] = Auth::user()->id;
+
+            $item->update($validated);
+
+            return response()->json([
+                'status' => 'sukses update data',
+                'data' => $item
+            ], 200);
+        } catch (Exception $e) {
+            return response()->json([
+                'status' => 'gagal update data',
+                'message' => $e->getMessage()
+            ], 500);
+        }
+    }
+
 
     public function destroy(Item $item)
     {
